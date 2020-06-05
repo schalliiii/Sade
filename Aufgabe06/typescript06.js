244027caab005f9ff445befaa62a0f231b6b79ce
Sade Babatunde <sade_babatunde@t-online.de>
2020-06-05 17:16:19 +0200

    hopefully done??

--- Aufgabe06/typescript06.js
+++ Aufgabe06/typescript06.js
@@ -1,63 +1,68 @@
 "use strict";
-var EUROPA = 4965.70 + 4616.40 + 4705.80 + 4615.20 + 4541.50 + 4436.90 + 4202 + 4191.60 + 4247.30 + 4278.70 + 4209.30;
-var NORDAMERIKA = 6600.40 + 6139.90 + 6375.80 + 6262.80 + 5998.90 + 6.167 + 6268.90 + 6048.70 + 5938.30 + 5920.50 + 6035.60;
-var SÜDAMERIKA = 1132.60 + 1132.60 + 1093.10 + 1164.20 + 1215.50 + 1.262 + 1308.90 + 1331.30 + 1321.70 + 1294.70 + 1279.20 + 1261.50;
-var AFRIKA = 1.028 + 1041.90 + 1072.10 + 1073.70 + 1107.10 + 1134.40 + 1167.10 + 1174.50 + 1192.10 + 1.207 + 1235.50;
-var ASIEN = 12954.70 + 13246.60 + 13986.80 + 14860.10 + 15308.80 + 15660.20 + 15787.70 + 15.877 + 15.984 + 16274.30 + 16274.10;
-var AUSTRALIEN = 1993.00 + 1875.70 + 1939.10 + 2035.20 + 2063.20 + 2014.70 + 2027.60 + 1986.80 + 1997.40 + 2001.30 + 2100.50;
-var WELT = EUROPA + NORDAMERIKA + SÜDAMERIKA + AFRIKA + ASIEN + AUSTRALIEN;
-var europaw = (EUROPA / WELT) * 100;
-var nordamerikaw = (NORDAMERIKA / WELT) * 100;
-var südamerikaw = (SÜDAMERIKA / WELT) * 100;
-var afrikaw = (AFRIKA / WELT) * 100;
-var asienw = (ASIEN / WELT) * 100;
-var australienw = (AUSTRALIEN / WELT) * 100;
-var EUROPE = (4209.30 - 4965.70) / 4965.70;
-var NORTHAMERICA = (6035.60 - 6600.40) / 6600.40;
-var SOUTHAMERICA = (1261.50 - 1132.60) / 1132.60;
-var AFRICA = (1235.50 - 1.028) / 1.028;
-var ASIA = (16274.10 - 12954.70) / 12954.70;
-var AUSTRALIA = (2100.50 - 1993.00) / 1993.00;
-var europe = 4209.30 - 4965.70;
-var northamerica = 6035.60 - 6600.40;
-var southamerica = 1261.50 - 1132.60;
-var africa = 1235.50 - 1.028;
-var asia = 16274.10 - 12954.70;
-var australia = 2100.50 - 1993.00;
-console.group("EUROPA");
-console.log("Die Emission von Europa ist: " + EUROPA + " kg CO2");
-console.log("Relativ zur Gesamtemission der Welt verursacht Europa damit " + europaw + " %");
-console.log("Für Europa hat sich 2018 im Vergleich zu 2008 die Emission um " + EUROPE + " % verändert");
-console.log("2018 im Vergleich zu 2008 sind das " + europe + " kg CO2");
-console.groupEnd();
-console.group("NORDAMERIKA");
-console.log("Die Emission von Nordamerika ist: " + NORDAMERIKA + " kg CO2");
-console.log("Relativ zur Gesamtemission der Welt verursacht Nordamerika damit " + nordamerikaw + " %");
-console.log("Für Nordamerika hat sich 2018 im Vergleich zu 2008 die Emission um  " + NORTHAMERICA + " % verändert");
-console.log("2018 im Vergleich zu 2008 sind das  " + northamerica + " kg CO2");
-console.groupEnd();
-console.group("SÜDAMERIKA");
-console.log("Die Emission von Südamerika ist:  " + SÜDAMERIKA + " kg CO2");
-console.log("Relativ zur Gesamtemission der Welt verursacht Südamerika damit " + südamerikaw + " %");
-console.log("Für Südamerika hat sich 2018 im Vergleich zu 2008 die Emission um " + SOUTHAMERICA + " % verändert");
-console.log("2018 im Vergleich zu 2008 sind das  " + southamerica + " kg CO2");
-console.groupEnd();
-console.group("AFRIKA");
-console.log("Die Emission von Afrika ist: " + AFRIKA + " kg CO2");
-console.log("Relativ zur Gesamtemission der Welt verursacht Afrika damit " + afrikaw + " %");
-console.log("Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um " + AFRICA + " % verändert");
-console.log("2018 im Vergleich zu 2008 sind das " + africa + " kg CO2");
-console.groupEnd();
-console.group("ASIEN");
-console.log("Die Emission von Asien ist: " + ASIEN + " kg CO2");
-console.log("Relativ zur Gesamtemission der Welt verursacht Asien damit " + asienw + " %");
-console.log("Für Asien hat sich 2018 im Vergleich zu 2008 die Emission um " + ASIA + " % verändert");
-console.log("2018 im Vergleich zu 2008 sind das " + asia + " kg CO2");
-console.groupEnd();
-console.group("AUSTRALIEN");
-console.log("Die Emission von Australien ist: " + AUSTRALIEN + " kg CO2");
-console.log("Relativ zur Gesamtemission der Welt verursacht Australien damit " + australienw + " %");
-console.log("Für Australien hat sich 2018 im Vergleich zu 2008 die Emission um " + AUSTRALIA + " % verändert");
-console.log("2018 im Vergleich zu 2008 sind das " + australia + " kg CO2");
-console.groupEnd();
+window.addEventListener("load", function ({ document, querySelector }) { }(".europe").addEventListener("click", EuropeFunction));
+function () { }
+'EuropeFunction'();
+{
+    document.getElementById("continent").innerHTML = "Europe";
+    document.getElementById("wort1").innerHTML = "Europe";
+    document.getElementById("Wert1").innerHTML = "4209.3";
+    document.getElementById("Wert2").innerHTML = "16.85577672348352";
+    document.getElementById("Wert3").innerHTML = "0.152324949151177";
+    document.getElementById("Wert4").innerHTML = "756.3999999999996";
+}
+document.querySelector(".northamerica").addEventListener("click", NorthamericaFunction);
+function () { }
+'NorthamericaFunction'();
+{
+    document.getElementById("continent").innerHTML = "Northamerica";
+    document.getElementById("wort1").innerHTML = "Northamerica";
+    document.getElementById("Wert1").innerHTML = "6035.6";
+    document.getElementById("Wert2").innerHTML = "21.184235729948313";
+    document.getElementById("Wert3").innerHTML = "0.08557057148051622";
+    document.getElementById("Wert4").innerHTML = "564.7999999999993";
+}
+document.querySelector(".southamerica").addEventListener("click", SouthamericaFunction);
+function () { }
+'SouthamericaFunction'();
+{
+    document.getElementById("continent").innerHTML = "Southamerica";
+    document.getElementById("wort1").innerHTML = "Southamerica";
+    document.getElementById("Wert1").innerHTML = "1261.5";
+    document.getElementById("Wert2").innerHTML = "4.6555275344741425";
+    document.getElementById("Wert3").innerHTML = "0.1138089351933605";
+    document.getElementById("Wert4").innerHTML = "128.9000000000001";
+}
+document.querySelector(".afrika").addEventListener("click", AfricaFunction);
+function () { }
+'AfricaFunction'();
+{
+    document.getElementById("continent").innerHTML = "Africa";
+    document.getElementById("wort1").innerHTML = "Africa";
+    document.getElementById("Wert1").innerHTML = "1235.5";
+    document.getElementById("Wert2").innerHTML = "3.5082273557806363";
+    document.getElementById("Wert3").innerHTML = "1200.8482490272372";
+    document.getElementById("Wert4").innerHTML = "1234.472";
+}
+document.querySelector(".asia").addEventListener("click", AsiaFunction);
+function () { }
+'AsiaFunction'();
+{
+    Document.getElementById("continent").innerHTML = "Asia";
+    Document.getElementById("wort1").innerHTML = "Asia";
+    Document.getElementById("Wert1").innerHTML = "16274.1";
+    Document.getElementById("Wert2").innerHTML = "46.218073485737406";
+    Document.getElementById("Wert3").innerHTML = "0.2562313291701081";
+    Document.getElementById("Wert4").innerHTML = "3319.3999999999996";
+}
+document.querySelector(".australia").addEventListener("click", AustraliaFunction);
+function () { }
+'AustraliaFunction'();
+{
+    Document.getElementById("continent").innerHTML = "Australia";
+    Document.getElementById("wort1").innerHTML = "Australia";
+    Document.getElementById("Wert1").innerHTML = "2100.5";
+    Document.getElementById("Wert2").innerHTML = "7.57815917057599";
+    Document.getElementById("Wert3").innerHTML = "0.05393878575012544";
+    Document.getElementById("Wert4").innerHTML = "107.5";
+}
 //# sourceMappingURL=typescript06.js.map
