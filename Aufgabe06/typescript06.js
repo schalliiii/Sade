0e6f4481f810a9150bc363fde25c2f9259988a77
Sade Babatunde <sade_babatunde@t-online.de>
2020-06-05 17:36:17 +0200

    .

--- Aufgabe06/typescript06.js
+++ Aufgabe06/typescript06.js
@@ -5,10 +5,10 @@ function () { }
 {
     document.getElementById("continent").innerHTML = "Europe";
     document.getElementById("wort1").innerHTML = "Europe";
-    document.getElementById("Wert1").innerHTML = "4209.3";
-    document.getElementById("Wert2").innerHTML = "16.85577672348352";
-    document.getElementById("Wert3").innerHTML = "0.152324949151177";
-    document.getElementById("Wert4").innerHTML = "756.3999999999996";
+    document.getElementById("Wert1").innerHTML = "4209.3 kg CO2";
+    document.getElementById("Wert2").innerHTML = "16.85577672348352 %";
+    document.getElementById("Wert3").innerHTML = "-0.152324949151177 %";
+    document.getElementById("Wert4").innerHTML = "-756.3999999999996 kg CO2";
 }
 document.querySelector(".northamerica").addEventListener("click", NorthamericaFunction);
 function () { }
@@ -16,10 +16,10 @@ function () { }
 {
     document.getElementById("continent").innerHTML = "Northamerica";
     document.getElementById("wort1").innerHTML = "Northamerica";
-    document.getElementById("Wert1").innerHTML = "6035.6";
-    document.getElementById("Wert2").innerHTML = "21.184235729948313";
-    document.getElementById("Wert3").innerHTML = "0.08557057148051622";
-    document.getElementById("Wert4").innerHTML = "564.7999999999993";
+    document.getElementById("Wert1").innerHTML = "6035.6 kg CO2";
+    document.getElementById("Wert2").innerHTML = "21.184235729948313 %";
+    document.getElementById("Wert3").innerHTML = "-0.08557057148051622 %";
+    document.getElementById("Wert4").innerHTML = "-564.7999999999993 kg CO2";
 }
 document.querySelector(".southamerica").addEventListener("click", SouthamericaFunction);
 function () { }
@@ -27,10 +27,10 @@ function () { }
 {
     document.getElementById("continent").innerHTML = "Southamerica";
     document.getElementById("wort1").innerHTML = "Southamerica";
-    document.getElementById("Wert1").innerHTML = "1261.5";
-    document.getElementById("Wert2").innerHTML = "4.6555275344741425";
-    document.getElementById("Wert3").innerHTML = "0.1138089351933605";
-    document.getElementById("Wert4").innerHTML = "128.9000000000001";
+    document.getElementById("Wert1").innerHTML = "1261.5 kg CO2";
+    document.getElementById("Wert2").innerHTML = "4.6555275344741425 %";
+    document.getElementById("Wert3").innerHTML = "0.1138089351933605 %";
+    document.getElementById("Wert4").innerHTML = "128.9000000000001 kg CO2";
 }
 document.querySelector(".afrika").addEventListener("click", AfricaFunction);
 function () { }
@@ -38,10 +38,10 @@ function () { }
 {
     document.getElementById("continent").innerHTML = "Africa";
     document.getElementById("wort1").innerHTML = "Africa";
-    document.getElementById("Wert1").innerHTML = "1235.5";
-    document.getElementById("Wert2").innerHTML = "3.5082273557806363";
-    document.getElementById("Wert3").innerHTML = "1200.8482490272372";
-    document.getElementById("Wert4").innerHTML = "1234.472";
+    document.getElementById("Wert1").innerHTML = "1235.5 kg CO2";
+    document.getElementById("Wert2").innerHTML = "3.5082273557806363 %";
+    document.getElementById("Wert3").innerHTML = "1200.8482490272372 %";
+    document.getElementById("Wert4").innerHTML = "1234.472 kg CO2";
 }
 document.querySelector(".asia").addEventListener("click", AsiaFunction);
 function () { }
@@ -49,10 +49,10 @@ function () { }
 {
     Document.getElementById("continent").innerHTML = "Asia";
     Document.getElementById("wort1").innerHTML = "Asia";
-    Document.getElementById("Wert1").innerHTML = "16274.1";
-    Document.getElementById("Wert2").innerHTML = "46.218073485737406";
-    Document.getElementById("Wert3").innerHTML = "0.2562313291701081";
-    Document.getElementById("Wert4").innerHTML = "3319.3999999999996";
+    Document.getElementById("Wert1").innerHTML = "16274.1 kg CO2";
+    Document.getElementById("Wert2").innerHTML = "46.218073485737406 %";
+    Document.getElementById("Wert3").innerHTML = "0.2562313291701081 %";
+    Document.getElementById("Wert4").innerHTML = "3319.3999999999996 kg CO2";
 }
 document.querySelector(".australia").addEventListener("click", AustraliaFunction);
 function () { }
@@ -60,9 +60,9 @@ function () { }
 {
     Document.getElementById("continent").innerHTML = "Australia";
     Document.getElementById("wort1").innerHTML = "Australia";
-    Document.getElementById("Wert1").innerHTML = "2100.5";
-    Document.getElementById("Wert2").innerHTML = "7.57815917057599";
-    Document.getElementById("Wert3").innerHTML = "0.05393878575012544";
-    Document.getElementById("Wert4").innerHTML = "107.5";
+    Document.getElementById("Wert1").innerHTML = "2100.5 kg CO2";
+    Document.getElementById("Wert2").innerHTML = "7.57815917057599 %";
+    Document.getElementById("Wert3").innerHTML = "0.05393878575012544 %";
+    Document.getElementById("Wert4").innerHTML = "107.5 kg CO2";
 }
 //# sourceMappingURL=typescript06.js.map
--- Aufgabe06/typescript06.ts
+++ Aufgabe06/typescript06.ts
@@ -6,10 +6,10 @@ function 'EuropeFunction'(); {
 
     document.getElementById("continent").innerHTML = "Europe";
     document.getElementById("wort1").innerHTML = "Europe";
-    document.getElementById("Wert1").innerHTML = "4209.3";
-    document.getElementById("Wert2").innerHTML = "16.85577672348352";
-    document.getElementById("Wert3").innerHTML = "0.152324949151177";
-    document.getElementById("Wert4").innerHTML = "756.3999999999996";
+    document.getElementById("Wert1").innerHTML = "4209.3 kg CO2";
+    document.getElementById("Wert2").innerHTML = "16.85577672348352 %";
+    document.getElementById("Wert3").innerHTML = "-0.152324949151177 %";
+    document.getElementById("Wert4").innerHTML = "-756.3999999999996 kg CO2";
 
 }
 document.querySelector(".northamerica").addEventListener("click", NorthamericaFunction);
@@ -18,10 +18,10 @@ function 'NorthamericaFunction'(); {
 
     document.getElementById("continent").innerHTML = "Northamerica";
     document.getElementById("wort1").innerHTML = "Northamerica";
-    document.getElementById("Wert1").innerHTML = "6035.6";
-    document.getElementById("Wert2").innerHTML = "21.184235729948313";
-    document.getElementById("Wert3").innerHTML = "0.08557057148051622";
-    document.getElementById("Wert4").innerHTML = "564.7999999999993";
+    document.getElementById("Wert1").innerHTML = "6035.6 kg CO2";
+    document.getElementById("Wert2").innerHTML = "21.184235729948313 %";
+    document.getElementById("Wert3").innerHTML = "-0.08557057148051622 %";
+    document.getElementById("Wert4").innerHTML = "-564.7999999999993 kg CO2";
 
 }
 document.querySelector(".southamerica").addEventListener("click", SouthamericaFunction);
@@ -30,10 +30,10 @@ function 'SouthamericaFunction'(); {
 
     document.getElementById("continent").innerHTML = "Southamerica";
     document.getElementById("wort1").innerHTML = "Southamerica";
-    document.getElementById("Wert1").innerHTML = "1261.5";
-    document.getElementById("Wert2").innerHTML = "4.6555275344741425";
-    document.getElementById("Wert3").innerHTML = "0.1138089351933605";
-    document.getElementById("Wert4").innerHTML = "128.9000000000001";
+    document.getElementById("Wert1").innerHTML = "1261.5 kg CO2";
+    document.getElementById("Wert2").innerHTML = "4.6555275344741425 %";
+    document.getElementById("Wert3").innerHTML = "0.1138089351933605 %";
+    document.getElementById("Wert4").innerHTML = "128.9000000000001 kg CO2";
 
 }
 document.querySelector(".afrika").addEventListener("click", AfricaFunction);
@@ -42,10 +42,10 @@ function 'AfricaFunction'(); {
 
     document.getElementById("continent").innerHTML = "Africa";
     document.getElementById("wort1").innerHTML = "Africa";
-    document.getElementById("Wert1").innerHTML = "1235.5";
-    document.getElementById("Wert2").innerHTML = "3.5082273557806363";
-    document.getElementById("Wert3").innerHTML = "1200.8482490272372";
-    document.getElementById("Wert4").innerHTML = "1234.472";
+    document.getElementById("Wert1").innerHTML = "1235.5 kg CO2";
+    document.getElementById("Wert2").innerHTML = "3.5082273557806363 %";
+    document.getElementById("Wert3").innerHTML = "1200.8482490272372 %";
+    document.getElementById("Wert4").innerHTML = "1234.472 kg CO2";
 
 }
 document.querySelector(".asia").addEventListener("click", AsiaFunction);
@@ -54,10 +54,10 @@ function 'AsiaFunction'(); {
 
     Document.getElementById("continent").innerHTML = "Asia";
     Document.getElementById("wort1").innerHTML = "Asia";
-    Document.getElementById("Wert1").innerHTML = "16274.1";
-    Document.getElementById("Wert2").innerHTML = "46.218073485737406";
-    Document.getElementById("Wert3").innerHTML = "0.2562313291701081";
-    Document.getElementById("Wert4").innerHTML = "3319.3999999999996";
+    Document.getElementById("Wert1").innerHTML = "16274.1 kg CO2";
+    Document.getElementById("Wert2").innerHTML = "46.218073485737406 %";
+    Document.getElementById("Wert3").innerHTML = "0.2562313291701081 %";
+    Document.getElementById("Wert4").innerHTML = "3319.3999999999996 kg CO2";
 
 }
 document.querySelector(".australia").addEventListener("click", AustraliaFunction);
@@ -66,10 +66,10 @@ function 'AustraliaFunction'(); {
 
     Document.getElementById("continent").innerHTML = "Australia";
     Document.getElementById("wort1").innerHTML = "Australia";
-    Document.getElementById("Wert1").innerHTML = "2100.5";
-    Document.getElementById("Wert2").innerHTML = "7.57815917057599";
-    Document.getElementById("Wert3").innerHTML = "0.05393878575012544";
-    Document.getElementById("Wert4").innerHTML = "107.5";
+    Document.getElementById("Wert1").innerHTML = "2100.5 kg CO2";
+    Document.getElementById("Wert2").innerHTML = "7.57815917057599 %";
+    Document.getElementById("Wert3").innerHTML = "0.05393878575012544 %";
+    Document.getElementById("Wert4").innerHTML = "107.5 kg CO2";
 
 }
 
