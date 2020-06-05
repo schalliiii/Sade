b17af920907820bac1afff29bf3a45dbbcce0e7e
Sade Babatunde <sade_babatunde@t-online.de>
2020-06-05 23:15:09 +0200

    so jetzt bin ich aber fertig hehe

--- Aufgabe06/typescript06.js
+++ Aufgabe06/typescript06.js
@@ -7,13 +7,13 @@ window.addEventListener('load', function () {
     document.querySelector(".asia").addEventListener('click', AsiaFunction);
     document.querySelector(".australia").addEventListener('click', AustraliaFunction);
     function EuropeFunction() {
-        document.getElementsById("continent").innerHTML = "Europe";
+        document.getElementById("continent").innerHTML = "Europe";
         document.getElementById("wort1").innerHTML = "Europe";
         document.getElementById("Wert1").innerHTML = "4209.3 kg CO2";
         document.getElementById("Wert2").innerHTML = "16.85577672348352 %";
         document.getElementById("Wert3").innerHTML = "-0.152324949151177 %";
         document.getElementById("Wert4").innerHTML = "-756.3999999999996 kg CO2";
-        document.querySelector(".chartWrapper .chart").setAttribute("style", "height: 16.85577672348352% ");
+        document.querySelector(".chartWrapper .chart").setAttribute("style", "height:16.8%");
     }
     function NorthamericaFunction() {
         document.getElementById("continent").innerHTML = "Northamerica";
@@ -22,7 +22,7 @@ window.addEventListener('load', function () {
         document.getElementById("Wert2").innerHTML = "21.184235729948313 %";
         document.getElementById("Wert3").innerHTML = "-0.08557057148051622 %";
         document.getElementById("Wert4").innerHTML = "-564.7999999999993 kg CO2";
-        document.querySelector(".chartWrapper .chart").setAttribute("style", "height: 21.184235729948313 % ");
+        document.querySelector(".chartWrapper .chart").setAttribute("style", "height:21.1%");
     }
     function SouthamericaFunction() {
         document.getElementById("continent").innerHTML = "Southamerica";
@@ -31,7 +31,7 @@ window.addEventListener('load', function () {
         document.getElementById("Wert2").innerHTML = "4.6555275344741425 %";
         document.getElementById("Wert3").innerHTML = "0.1138089351933605 %";
         document.getElementById("Wert4").innerHTML = "128.9000000000001 kg CO2";
-        document.querySelector(".chartWrapper .chart").setAttribute("style", "height: 4.6555275344741425 % ");
+        document.querySelector(".chartWrapper .chart").setAttribute("style", "height:4.7%");
     }
     function AfricaFunction() {
         document.getElementById("continent").innerHTML = "Africa";
@@ -40,7 +40,7 @@ window.addEventListener('load', function () {
         document.getElementById("Wert2").innerHTML = "3.5082273557806363 %";
         document.getElementById("Wert3").innerHTML = "1200.8482490272372 %";
         document.getElementById("Wert4").innerHTML = "1234.472 kg CO2";
-        document.querySelector(".chartWrapper .chart").setAttribute("style", "height: 3.5082273557806363 %  ");
+        document.querySelector(".chartWrapper .chart").setAttribute("style", "height:3.5%");
     }
     function AsiaFunction() {
         document.getElementById("continent").innerHTML = "Asia";
@@ -49,7 +49,7 @@ window.addEventListener('load', function () {
         document.getElementById("Wert2").innerHTML = "46.218073485737406 %";
         document.getElementById("Wert3").innerHTML = "0.2562313291701081 %";
         document.getElementById("Wert4").innerHTML = "3319.3999999999996 kg CO2";
-        document.querySelector(".chartWrapper .chart").setAttribute("style", "height: 46.218073485737406 %  ");
+        document.querySelector(".chartWrapper .chart").setAttribute("style", "height:46.3%");
     }
     function AustraliaFunction() {
         document.getElementById("continent").innerHTML = "Australia";
@@ -58,7 +58,7 @@ window.addEventListener('load', function () {
         document.getElementById("Wert2").innerHTML = "7.57815917057599 %";
         document.getElementById("Wert3").innerHTML = "0.05393878575012544 %";
         document.getElementById("Wert4").innerHTML = "107.5 kg CO2";
-        document.querySelector(".chartWrapper .chart").setAttribute("style", "height: 7.57815917057599 %  ");
+        document.querySelector(".chartWrapper .chart").setAttribute("style", "height:7.6%");
     }
 });
 //# sourceMappingURL=typescript06.js.map
