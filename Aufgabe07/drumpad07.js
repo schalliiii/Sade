4a19713e89aa690498442999b68d2d60ae93f5d3
Sade Babatunde <sade_babatunde@t-online.de>
2020-06-12 14:59:04 +0200

    aufgabe07

---
+++ Aufgabe07/drumpad07.js
@@ -0,0 +1,14 @@
+"use strict";
+window.addEventListener('load', function () {
+    var allSounds = ["A.mp3", "C.mp3", "F.mp3", "G.mp3", "hihat.mp3", "kick.mp3", "laugh-1.mp3", "laugh-2.mp3", "snare.mp3"];
+    document.querySelector("#b1").addEventListener("click", function () { playSample(0); });
+    document.querySelector("#b2").addEventListener("click", function () { playSample(1); });
+    document.querySelector("#b3").addEventListener("click", function () { playSample(2); });
+    document.querySelector("#b4").addEventListener("click", function () { playSample(3); });
+    document.querySelector("#b5").addEventListener("click", function () { playSample(4); });
+    document.querySelector("#b6").addEventListener("click", function () { playSample(5); });
+    document.querySelector("#b7").addEventListener("click", function () { playSample(6); });
+    document.querySelector("#b8").addEventListener("click", function () { playSample(7); });
+    document.querySelector("#b9").addEventListener("click", function () { playSample(8); });
+});
+//# sourceMappingURL=drumpad07.js.map
---
+++ Aufgabe07/drumpad07.ts
@@ -0,0 +1,40 @@
+
+function playSample( mp3 : string) {
+    var allSounds:HTMLAudioElement = new Audio( mp3);
+    allSounds.play();
+}
+
+window.addEventListener('load', function() {
+
+document.querySelector("#b1").addEventListener("click", function() {
+    playSample("A.mp3") 
+ });
+document.querySelector("#b2").addEventListener("click", function() {
+    playSample("C.mp3") 
+ });
+ document.querySelector("#b3").addEventListener("click", function() {
+    playSample("F.mp3") 
+});
+document.querySelector("#b4").addEventListener("click", function() {
+    playSample("G.mp3")
+});
+document.querySelector("#b5").addEventListener("click", function() {
+    playSample("hihat.mp3")
+});
+document.querySelector("#b6").addEventListener("click", function() {
+    playSample("kick.mp3")
+});
+document.querySelector("#b7").addEventListener("click", function() {
+    playSample("laugh-1.mp3")
+});
+document.querySelector("#b8").addEventListener("click", function() {
+    playSample("laugh-2.mp3")
+});
+document.querySelector("#b9").addEventListener("click", function() {
+    playSample("snare.mp3")
+});
+
+ } )
+
+ 
+
