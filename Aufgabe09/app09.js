1a2b765ed3e5890a54f2f6a687a4b11d0fe0999e
Sade Babatunde <sade_babatunde@t-online.de>
2020-06-28 11:36:41 +0200

    test

--- Aufgabe09/app09.js
+++ Aufgabe09/app09.js
@@ -1,22 +1,28 @@
-// Aufgabe 9 
-var myList = ["einkaufen", "essen", "eia schaffen"];
-console.log(myList);
-document.querySelector(".todoliste").innerHTML = myList[0];
-document.querySelector(".todoliste").innerHTML += myList[1];
-document.querySelector(".todoliste").innerHTML += myList[2];
-function drawList() {
-    document.querySelector(".todoliste").innerHTML = "";
-    for (var index = 0; (index < myList.length); index++) {
-        console.log(index);
-        document.querySelector(".todoliste").innerHTML += "<li>" + myList[index];
-    }
-}
-function addNewElement() {
-    myList.push("BWL Hausarbeit");
-    drawList();
-}
+// Aufgabe 9 mit Unterstützung und Tipps von Herr Rausch und Naomi Noller
+// Array erstellen
+var mytodoList = ["Putzen", "Medienpsychologie nachholen", "Spazieren gehen", "EIA Aufgabe lösen"];
 window.addEventListener("load", function () {
+    // Array Elemente werden ausgegeben und der Papierkorb wird angezeigt
+    function drawList() {
+        listnr2.innerHTML = "";
+        for (var index = 0; index < mytodoList.length; index++) {
+            listnr2.innerHTML += "<div>" + "<input type='checkbox'>" + mytodoList[index] + "<i class='fas fa-trash-alt'id=delete></i>" + "</div>";
+        }
+        // "in total" Anzeige passt sich dadurch automatisch an
+        var intotal = document.querySelector("#Span");
+        intotal.innerHTML = mytodoList.length;
+    }
+    var listnr2 = document.querySelector(".addlist");
     drawList();
-    addNewElement();
+    // Mit klick auf add-button1 wird ein neues Element hinzugefügt
+    var eingabe = document.querySelector("#newAdd");
+    console.log(eingabe);
+    var addNewElement = document.querySelector("#button");
+    addNewElement.addEventListener("click", function () {
+        mytodoList.push(eingabe.value);
+        drawList();
+        eingabe.value = "";
+        console.log(mytodoList);
+    });
 });
 //# sourceMappingURL=app09.js.map
--- Aufgabe09/app09.ts
+++ Aufgabe09/app09.ts
@@ -1,38 +1,38 @@
-// Aufgabe 9 
+// Aufgabe 9 mit Unterstützung und Tipps von Herr Rausch und Naomi Noller
+
+// Array erstellen
+let mytodoList: string[] = ["Putzen",  "Medienpsychologie nachholen", "Spazieren gehen", "EIA Aufgabe lösen"];
 
 
-let myList: string[] = ["einkaufen", "essen", "eia schaffen"];
-
-console.log(myList);
-
-document.querySelector(".todoliste").innerHTML = myList[0];
-document.querySelector(".todoliste").innerHTML += myList[1];
-document.querySelector(".todoliste").innerHTML += myList[2];
-
-function drawList(): void {
-
-    document.querySelector(".todoliste").innerHTML = "";
-    for (let index: number = 0;
-        (index < myList.length; index++) {
-
-        console.log(index);
-        document.querySelector(".todoliste").innerHTML += "<li>" + myList[index];
-
+window.addEventListener("load", function () {
+    // Array Elemente werden ausgegeben und der Papierkorb wird angezeigt
+    function drawList(): void {
+        listnr2.innerHTML = "";
+        for (var index: number = 0;
+            index < mytodoList.length; index++) {
+             listnr2.innerHTML += "<div>" + "<input type='checkbox'>" + mytodoList[index] + "<i class='fas fa-trash-alt'id=delete></i>" + "</div>";
+        }
+        // "in total" Anzeige passt sich dadurch automatisch an
+        var intotal = document.querySelector("#Span");
+        intotal.innerHTML = mytodoList.length;
     }
 
-}
 
-
-
-
-function addNewElement(): void {
-    myList.push("BWL Hausarbeit");
+   var listnr2:string = document.querySelector(".addlist");
     drawList();
-}
-window.addEventListener("load", function () {
+    // Mit klick auf add-button1 wird ein neues Element hinzugefügt
+    var eingabe: string = document.querySelector("#newAdd");
+    console.log(eingabe);
+    var addNewElement: string = document.querySelector("#button");
+    addNewElement.addEventListener("click", function () {
+        mytodoList.push(eingabe.value);
+        drawList();
+        eingabe.value = "";
+        console.log(mytodoList);
+    });
+});
 
-    drawList();
-    addNewElement();
 
 
-})
+
+
