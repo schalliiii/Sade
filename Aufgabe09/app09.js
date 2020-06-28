b94bb8563b374c149d6feabf5537feae87315130
Sade Babatunde <sade_babatunde@t-online.de>
2020-06-28 12:16:00 +0200

    hoffentlich klappt alles

--- Aufgabe09/app09.js
+++ Aufgabe09/app09.js
@@ -4,15 +4,15 @@ var mytodoList = ["Putzen", "Medienpsychologie nachholen", "Spazieren gehen", "E
 window.addEventListener("load", function () {
     // Array Elemente werden ausgegeben und der Papierkorb wird angezeigt
     function drawList() {
-        listnr2.innerHTML = "";
+        zweiteliste.innerHTML = "";
         for (var index = 0; index < mytodoList.length; index++) {
-            listnr2.innerHTML += "<div>" + "<input type='checkbox'>" + mytodoList[index] + "<i class='fas fa-trash-alt'id=delete></i>" + "</div>";
+            zweiteliste.innerHTML += "<div>" + "<input type='checkbox'>" + mytodoList[index] + "<i class='fas fa-trash-alt'id=delete></i>" + "</div>";
         }
         // "in total" Anzeige passt sich dadurch automatisch an
         var intotal = document.querySelector("#Span");
         intotal.innerHTML = mytodoList.length;
     }
-    var listnr2 = document.querySelector(".addlist");
+    var zweiteliste = document.querySelector(".addtolist");
     drawList();
     // Mit klick auf add-button1 wird ein neues Element hinzugefügt
     var eingabe = document.querySelector("#newAdd");
--- Aufgabe09/app09.ts
+++ Aufgabe09/app09.ts
@@ -1,24 +1,24 @@
 // Aufgabe 9 mit Unterstützung und Tipps von Herr Rausch und Naomi Noller
 
 // Array erstellen
-let mytodoList: string[] = ["Putzen",  "Medienpsychologie nachholen", "Spazieren gehen", "EIA Aufgabe lösen"];
+let mytodoList: string[] = ["Putzen", "Medienpsychologie nachholen", "Spazieren gehen", "EIA Aufgabe lösen"];
 
 
 window.addEventListener("load", function () {
     // Array Elemente werden ausgegeben und der Papierkorb wird angezeigt
     function drawList(): void {
-        listnr2.innerHTML = "";
+        zweiteliste.innerHTML = "";
         for (var index: number = 0;
             index < mytodoList.length; index++) {
-             listnr2.innerHTML += "<div>" + "<input type='checkbox'>" + mytodoList[index] + "<i class='fas fa-trash-alt'id=delete></i>" + "</div>";
+            zweiteliste.innerHTML += "<div>" + "<input type='checkbox'>" + mytodoList[index] + "<i class='fas fa-trash-alt'id=delete></i>" + "</div>";
         }
         // "in total" Anzeige passt sich dadurch automatisch an
-        var intotal = document.querySelector("#Span");
+        var intotal: string[] = document.querySelector("#Span");
         intotal.innerHTML = mytodoList.length;
     }
 
 
-   var listnr2:string = document.querySelector(".addlist");
+    var zweiteliste = document.querySelector(".addtolist");
     drawList();
     // Mit klick auf add-button1 wird ein neues Element hinzugefügt
     var eingabe: string = document.querySelector("#newAdd");
